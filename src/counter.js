bb135ad8b61e9a3ac4dbc03d52bdeadbfdfbd65a
Alex Deng <alex.deng2002@gmail.com>
2023-05-23 10:53:38 -0500

    changed html tags to tailwindcss

--- src/counter.js
+++ src/counter.js
@@ -1,33 +1,32 @@
 import React, { useState } from 'react';
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    const countUp = () => {
-        setCount(count + 1);
-    };
+  const countUp = () => {
+    setCount(count + 1);
+  };
 
-    const countDown = () => {
-        setCount(count - 1);
-    };
+  const countDown = () => {
+    setCount(count - 1);
+  };
 
-    return (
-        <div style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <h2 className='font-bold text-purple-500 text-9xl'>Counter App</h2>
-            <div style={{ margin: '20px' }}></div>
-            <p className='text-5xl font-medium text-blue-500'>Current Count: {count}</p>
-            <div style={{ margin: '20px' }}></div>
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
-                <div style={{ borderRadius: '10px', backgroundColor: 'yellow', padding: '20px' }}>
-                    <button className='font-mono text-3xl text-red-500' onClick={countDown}>-</button>
-                </div>
-                <div style={{ borderRadius: '10px', backgroundColor: 'yellow', padding: '20px' }}>
-                    <button className='font-mono text-3xl text-green-500' onClick={countUp}>+</button>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col items-center bg-white">
+      <h2 className="font-bold text-purple-500 text-9xl">Counter App</h2>
+      <div className="my-8"></div>
+      <p className="text-5xl font-medium text-blue-500">Current Count: {count}</p>
+      <div className="my-8"></div>
+      <div className="flex justify-between w-52">
+        <div className="p-4 bg-yellow-300 border border-orange-400 rounded-lg">
+          <button className="text-3xl text-red-500" onClick={countDown}>-</button>
         </div>
-
-    );
+        <div className="p-4 bg-yellow-300 border border-orange-400 rounded-lg">
+          <button className="text-3xl text-green-500" onClick={countUp}>+</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
