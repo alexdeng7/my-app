9e50fc0cafbf69b5bfd6134aa948012a644c0c39
Alex Deng <alex.deng2002@gmail.com>
2023-05-23 13:29:05 -0500

    using useEffect count to 9 reset to 0

--- src/counter.js
+++ src/counter.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const Counter = ({ defaultValue }) => {
+  const [count, setCount] = useState(defaultValue);
+
+  useEffect(() => {
+    if (count > 9) {
+      setCount(0);
+    }
+  }, [count]);
 
   const countUp = () => {
     setCount(count + 1);
@@ -15,14 +21,20 @@ const Counter = () => {
     <div className="flex flex-col items-center bg-white">
       <h2 className="font-bold text-purple-500 text-9xl">Counter App</h2>
       <div className="my-8"></div>
-      <p className="text-5xl font-medium text-blue-500">Current Count: {count}</p>
+      <p className="text-5xl font-medium text-blue-500">
+        Current Count: {count}
+      </p>
       <div className="my-8"></div>
       <div className="flex justify-between w-52">
         <div className="p-4 bg-yellow-300 border border-orange-400 rounded-lg">
-          <button className="text-3xl text-red-500" onClick={countDown}>-</button>
+          <button className="text-3xl text-red-500" onClick={countDown}>
+            -
+          </button>
         </div>
         <div className="p-4 bg-yellow-300 border border-orange-400 rounded-lg">
-          <button className="text-3xl text-green-500" onClick={countUp}>+</button>
+          <button className="text-3xl text-green-500" onClick={countUp}>
+            +
+          </button>
         </div>
       </div>
     </div>
