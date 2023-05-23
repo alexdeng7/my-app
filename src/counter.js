fc44249e6de764b5360856bfdd4992eac44ea23b
Alex Deng <alex.deng2002@gmail.com>
2023-05-23 10:30:07 -0500

    Final Edits

--- src/counter.js
+++ src/counter.js
@@ -1,24 +1,33 @@
 import React, { useState } from 'react';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const countUp = () => {
-    setCount(count + 1);
-  };
+    const countUp = () => {
+        setCount(count + 1);
+    };
 
-  const countDown = () => {
-    setCount(count - 1);
-  };
+    const countDown = () => {
+        setCount(count - 1);
+    };
 
-  return (
-    <div style={{ backgroundColor: 'white' }}>
-      <h2 className='text-purple-500 text-9xl'>Counter App</h2>
-      <p className='text-5xl text-blue-500'>Current Count: {count}</p>
-      <button className='text-lg text-red-500' onClick={countDown}>-</button>
-      <button className='text-lg text-red-500' onClick={countUp}>+</button>
-    </div>
-  );
+    return (
+        <div style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <h2 className='font-bold text-purple-500 text-9xl'>Counter App</h2>
+            <div style={{ margin: '20px' }}></div>
+            <p className='text-5xl font-medium text-blue-500'>Current Count: {count}</p>
+            <div style={{ margin: '20px' }}></div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
+                <div style={{ borderRadius: '10px', backgroundColor: 'yellow', padding: '20px' }}>
+                    <button className='font-mono text-3xl text-red-500' onClick={countDown}>-</button>
+                </div>
+                <div style={{ borderRadius: '10px', backgroundColor: 'yellow', padding: '20px' }}>
+                    <button className='font-mono text-3xl text-green-500' onClick={countUp}>+</button>
+                </div>
+            </div>
+        </div>
+
+    );
 };
 
 export default Counter;
