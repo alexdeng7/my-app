2f37ed0f4756386aefeb882c9e645777159680f6
Alex Deng <alex.deng2002@gmail.com>
2023-05-24 11:41:39 -0500

    Counter update with get users and posts

--- src/counter.js
+++ src/counter.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 
 const Counter = ({ defaultValue }) => {
   const [count, setCount] = useState(defaultValue);
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [activeSection, setActiveSection] = useState(null);
 
   useEffect(() => {
-    if (count > 9) {
+    if (count > 9 || count < -9) {
       setCount(0);
     }
   }, [count]);
@@ -15,6 +18,39 @@ const Counter = ({ defaultValue }) => {
 
   const countDown = () => {
     setCount(count - 1);
+  };
+
+  const getPosts = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+        setActiveSection("posts");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const getUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+        setActiveSection("users");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const clearPage = () => {
+    setCount(defaultValue);
+    setPosts([]);
+    setUsers([]);
+    setActiveSection(null);
   };
 
   return (
@@ -36,6 +72,56 @@ const Counter = ({ defaultValue }) => {
             +
           </button>
         </div>
+      </div>
+      <div className="p-4 mt-4 border border-orange-400 rounded-lg bg-amber-200 w-72">
+        <button className="text-3xl text-rose-300" onClick={getPosts}>
+          Get Posts
+        </button>
+      </div>
+      <div className="p-4 mt-4 border border-orange-400 rounded-lg bg-amber-200 w-72">
+        <button className="text-3xl text-rose-300" onClick={getUsers}>
+          Get Users
+        </button>
+      </div>
+      {activeSection === "posts" && (
+        <div id="output" className="mt-8">
+          {posts.map((post) => (
+            <div
+              key={post.id}
+              className="p-6 mb-6 text-black bg-white rounded-lg shadow-lg"
+            >
+              <h3 className="text-2xl font-bold uppercase">{post.title}</h3>
+              <p className="mt-4 text-lg font-light">{post.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      {activeSection === "users" && (
+        <div id="output" className="mt-8">
+          {users.map((user) => (
+            <div
+              key={user.id}
+              className="p-6 mb-6 text-black bg-white rounded-lg shadow-lg"
+            >
+              <ul>
+                <li className="text-2xl font-bold uppercase">
+                  Username: {user.username}
+                </li>
+                <li className="mt-4 text-lg font-light">ID: {user.id}</li>
+                <li className="mt-4 text-lg font-light">Name: {user.name}</li>
+                <li className="mt-4 text-lg font-light">Email: {user.email}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+      <div className="mt-8">
+        <button
+          className="text-lg text-gray-500 hover:text-red-500"
+          onClick={clearPage}
+        >
+          Return To Default
+        </button>
       </div>
     </div>
   );
